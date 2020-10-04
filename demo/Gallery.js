d5444609edad4940944f8e5f25497171ab3d5fab
“Anudeep <anudeep.medishetti@stonybrook.edu>
2020-10-03 22:39:56 -0400

    Color change

--- demo/Gallery.js
+++ demo/Gallery.js
@@ -27,21 +27,31 @@ var Gallery = function (evt, m, options) {
             frag = document.createDocumentFragment(),
             li = document.createElement('li'),
             a = document.createElement('a'),
-            img = document.createElement('img');
+            canvas = document.createElement('canvas'),
+            img = document.createElement('img'),
+            img1 = document.createElement("img");
 
         for (i; i < len; i += 1) {
             li = li.cloneNode(false);
             a = a.cloneNode(false);
+            canvas = canvas.cloneNode(false);
             img = img.cloneNode(false);
-
+            img1 = img1.cloneNode(false);
+            console.log(img.src);
+            //img.src = this.changeColor(canvas,img).toDataURL();
+            console.log(img.src);
+            this.changeColor(canvas,img,img1,a,i).toDataURL();
             a.href = options.images[i].url;
             a.title = options.images[i].title;
             img.src = options.images[i].thumb;
+            //img1.src = options.images[i].thumb;
             img.setAttribute('data-large-img-url', options.images[i].large);
             img.className = 'img';
             img.id = 'img-' + i;
-
-            a.appendChild(img);
+            
+            
+            
+            a.appendChild(img1);
             li.appendChild(a);
 
             frag.appendChild(li);
@@ -52,10 +62,60 @@ var Gallery = function (evt, m, options) {
         walk(1);
 
         m.attach({
-            thumb: '.img',
+            thumb: '.img1',
             zoomable: true
         });
     };
+
+    this.changeColor = function(canvas, img,img1,a,j){
+        var ctx= canvas.getContext("2d");
+       
+        img.crossOrigin = "anonymous";
+        // When the image has loaded
+        img.onload = function(){
+            // Draw it and get it's data
+            console.log("Hello");
+            canvas.width=img.width;
+            canvas.height=img.height;
+            var w = canvas.width / 3;
+            ctx.drawImage(img, 0, 0);
+            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height),
+                data = imgData.data;
+
+            // Iterate over all the pixels
+            for (var i = 0; i < data.length; i += 4) {
+                // The current pixels' x position
+                var x = Math.floor(i / 4) % canvas.width;
+
+                //Set the pixels' r, g, b channels if x is within certain bounds.
+                if(x < w)
+                    data[i] = 255;   // Red
+                else if(x >= w && x < w * 2)
+                    data[i+1] = 255; // Green
+                else
+                    data[i+2] = 255; // Blue
+            }
+
+            // Re-draw the image.
+            ctx.putImageData(imgData, 0, 0);
+
+            
+            img1.src = canvas.toDataURL();
+            console.log(options.images[j]);
+            img1.setAttribute('data-large-img-url', img1.src);
+            img1.className = 'img1';
+            img1.id = 'img1-' + j;
+            m.attach({
+                thumb: '.img1',
+                zoomable: true
+            });
+            
+                
+        }
+        
+        //console.log(ctx.getImageData(0,0,canvas.width,canvas.height));
+        return canvas;
+    }
 
     this.next = function () {
         walk(1);
@@ -64,9 +124,13 @@ var Gallery = function (evt, m, options) {
     this.prev = function () {
         walk(-1);
     };
+    this.change = function(){
+        console.log("In the change");
+    }
 
     evt.attach('mousedown', options.prev, this.prev);
     evt.attach('mousedown', options.next, this.next);
+    evt.attach('mousedown', options.change, this.change);
 
     this.render();
 };
